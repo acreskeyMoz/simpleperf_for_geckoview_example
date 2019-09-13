b42e5024a2835fa356ab0f182cca24d538b90211
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2019-09-13 09:33:21 -0400

    Update copy_libs.sh

--- copy_libs.sh
+++ copy_libs.sh
@@ -5,8 +5,3 @@
 : SIMPLEPERF_PATH ${SIMPLEPERF_PATH:=/Users/acreskey/tools/simpleperf/repo/test_repo/simpleperf/binary_cache/data/app/org.mozilla.geckoview_example-1/lib/arm}
 
 cp $LOCAL_BUILD_PATH/dist/bin/*.so $SIMPLEPERF_PATH
-cp $LOCAL_BUILD_PATH/dist/bin/libmozglue.so $SIMPLEPERF_PATH
-cp $LOCAL_BUILD_PATH/dist/bin/libnssckbi.so $SIMPLEPERF_PATH
-cp $LOCAL_BUILD_PATH/security/libnss3.so $SIMPLEPERF_PATH
-cp $LOCAL_BUILD_PATH/security/nss/lib/softoken/softoken_softokn3/libsoftokn3.so $SIMPLEPERF_PATH
-cp $LOCAL_BUILD_PATH/security/nss/lib/freebl/freebl_freebl3/libfreebl3.so $SIMPLEPERF_PATH
