01dca245af2a257b2a4901f39bb7b02e17e8d7f3
Andrew Creskey <44072237+acreskeyMoz@users.noreply.github.com>
2019-09-24 15:50:54 -0400

    Fix to issue where some devices would include different gv folders
    Thank you @jesup

--- copy_libs.sh
+++ copy_libs.sh
@@ -2,6 +2,6 @@
 
 # Copy shared objects with symbols from local build to simpleperf binary_cache
 : LOCAL_BUILD_PATH ${LOCAL_BUILD_PATH:=/Users/acreskey/dev/firefox/src/build/obj-release-android}
-: SIMPLEPERF_PATH ${SIMPLEPERF_PATH:=/Users/acreskey/tools/simpleperf/repo/test_repo/simpleperf/binary_cache/data/app/org.mozilla.geckoview_example-1/lib/arm}
+: SIMPLEPERF_PATH ${SIMPLEPERF_PATH:=/Users/acreskey/tools/simpleperf/repo/test_repo/simpleperf/binary_cache/data/app/org.mozilla.*/lib/arm}
 
 cp $LOCAL_BUILD_PATH/dist/bin/*.so $SIMPLEPERF_PATH
